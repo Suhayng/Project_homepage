f01c9eb3560c2d49a451fe6112f272f8d325cba2
Suhyang Gwon <znsl65@naver.com>
2022-04-03 01:53:13 +0900

    [update] 2022.04.03

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var showImg, nextImg, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16,
 n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,n16;
+
 function fadeInOut(){
    showImg = $(".new1 img:eq(0)");
    s2 = $(".new2 img:eq(0)");
